62e2e2d6b8fb00b5781d3d4315ec20827569f1ba
Anwar Dabbour <anwar.dabor@gmail.com>
2026-06-10 18:56:55 +0300

    init templateMS

---
+++ templateMS/src/main/java/com/alerthub/nameofmicroservices/AlertHubApplication.java
@@ -0,0 +1,13 @@
+package com.alerthub.nameofmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertHubApplication {
+//first commit
+    public static void main(String[] args) {
+        SpringApplication.run(AlertHubApplication.class, args);
+    }
+
+}
---
+++ templateMS/src/test/java/com/alerthub/nameofmicroservices/AlertHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alerthub.nameofmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
