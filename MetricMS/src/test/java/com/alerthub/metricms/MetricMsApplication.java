57be5ca5e187979e615c2aa56750167604938d16
ayleen5 <ayleenm55@gmail.com>
2026-06-29 09:46:03 +0300

    Metric MS progress

---
+++ MetricMS/src/test/java/com/alerthub/metricms/MetricMsApplication.java
@@ -0,0 +1,13 @@
+package com.alerthub.nameofmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricMsApplication {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
